673eb020a7fa7716bedb4a5922286c9712bb5924
Brendan Matteoli <brendanmatteoli@gmail.com>
2020-02-26 21:56:21 -0500

    added smaller star list

--- app/src/main/java/com/mount/mountmobileapp/TrackActivity.kt
+++ app/src/main/java/com/mount/mountmobileapp/TrackActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_track.*
+import java.io.InputStream
 
 class TrackActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_track)
+
+        //code to load database here
+        val starListInputStream = resources.openRawResource(R.raw.hygdata_v3)
 
         buttonStart.setOnClickListener {
             val intent = Intent(this@TrackActivity, MainActivity::class.java)
